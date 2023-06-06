10c6b47dfdd3ee297ee719b5830dbd7d11507f24
Joshua Corley <corleyjoshua@gmail.com>
2023-06-05 20:15:42 -0400

    SPI State Machine Update
    (1) Updated the state machine to output in correct format.
    (2) Currently setup to only work with the example OLED code. Change will happen in the future for the OLED code.

--- SPI.sv
+++ SPI.sv
@@ -1,101 +1,112 @@
 `timescale 1ns / 1ps
 
-module spi_controller #(
-	parameter SCLK_DUTY_CYCLE = 5
-	) (
-	input        sysClkIn,
-	input        sysRstIn,
-	input        tValidIn,
-	output       tReadyOut,
-	input  [7:0] tDataIn,
-	output       SCLK,
-	output       SS,
-	output       MOSI,
-	input        MISO
+module spi_intf #(
+  parameter SCLK_DUTY_CYCLE = 5
+) (
+  // Clocks, Resets
+  input  clkIn,
+  input  rstNIn,
+  output spiClkOut,
+  input  spiRstNIn,
+
+  // Data Input
+  AXI4_STREAM_BASIC.SLAVE sAxiS,
+
+  // SPI Interface
+  input  MISO,
+  output MOSI,
+  output SS
 );
 
-	localparam COUNTER_MID = SCLK_DUTY_CYCLE - 1;
-	localparam COUNTER_MAX = (SCLK_DUTY_CYCLE*2)-1;
+  localparam COUNTER_MID = SCLK_DUTY_CYCLE-1;
+  localparam COUNTER_MAX = (SCLK_DUTY_CYCLE*2)-1;
 
-	typedef enum {IDLE, SEND, HOLD_SS, HOLD} spi_enum;
+  typedef enum {
+    IDLE,
+    SEND
+  } spi_sm_enum;
 
-	spi_enum    state;
-	logic [7:0] shiftReg;
-	logic [3:0] shiftCounter;
-	logic [4:0] counter;
-	logic       clkDiv;
-	logic       mosiTemp;
+  (* mark_debug = "true" *) spi_sm_enum spiState;
+  logic [4:0] counter = 0;
+  logic mosiR;
+  logic ssR;
+  logic tReadyR;
+  logic [7:0] shiftData;
+  logic [3:0] shiftCounter;
 
-	// State Machine for sending out 8-bit SPI transactions.
-	always@(posedge sysRstIn or posedge sysClkIn)
-	begin
-		if (sysRstIn) begin
-			shiftReg     <= 0;
-			shiftCounter <= 0;
-			mosiTemp     <= 1'b1;
-			state        <= IDLE;
-		end else begin
-			case (state)
-				IDLE: begin
-					shiftCounter <= 0;
-					shiftReg     <= tDataIn;
-					mosiTemp     <= 1'b1;
+  /*
+  SPI Clock Generator
+  Generates the SPI clock at the requested duty cycle when the counter reaches the mid-point
+  and it's not the end of the data sequence.
+  */
+  always_ff @ (posedge clkIn or negedge rstNIn)
+  begin
+    if (!rstNIn)
+    begin
+      counter <= 'b0;
+    end else if (spiState == SEND && ~(counter == COUNTER_MID && shiftCounter == 8))
+    begin
+      if (counter == COUNTER_MAX)
+        counter <= 'b0;
+      else
+        counter <= counter + 'b1;
+    end else
+      counter <= 'b0;
+  end
 
-					if (tValidIn == 1'b1)
-						state <= SEND;
-				end
+  /*
+  SPI Generation
+  spiClkOut generated from counter being less than the duty cycle and the slave is selected
+  MOSI is high when mosiQ is high and slave is selected else it's 0
+  SS is high when not in the SEND state
+  tReadyOut is high when in the IDLE state and tValidIn is low
+  TODO : The tValidIn condition is because of the OLED control state machine. This will change in the future.
+  */
+  assign SS           = (spiState != SEND) ? 'b1 : 'b0;
+  assign MOSI         = mosiR | SS ? 'b1 : 'b0;
+  assign spiClkOut    = (counter < SCLK_DUTY_CYCLE) | SS;
+  assign sAxiS.tready = (spiState == IDLE && sAxiS.tvalid == 'b0) ? 'b1 : 'b0;
 
-				SEND: begin
-					if (counter == COUNTER_MID) begin
-						mosiTemp <= shiftReg[7];
-						shiftReg <= {shiftReg[6:0], 1'b0};
-						if (shiftCounter == 4'd8)
-							shiftCounter <= 1'b0;
-						else
-							shiftCounter <= shiftCounter + 1;
-					end
+  always_ff @ (posedge clkIn or negedge rstNIn)
+  begin
+    if (!rstNIn)
+    begin
+      mosiR        <= 'b1;
+//      sAxiS.tready <= 'b0;
+      shiftCounter <= 'b0;
+      spiState     <= IDLE;
+    end else
+    begin
+      case (spiState)
+        IDLE: begin
+          mosiR        <= 'b1;
+//          sAxiS.tready <= 'b1;
 
-					if (shiftCounter == 4'd8 && counter == COUNTER_MID)
-						state <= HOLD_SS;
-				end
+          if (sAxiS.tvalid == 'b1)
+          begin
+//            sAxiS.tready <= 'b1;
+            shiftData    <= sAxiS.tdata;
+            spiState     <= SEND;
+          end
+        end
 
-				HOLD_SS: begin
-					shiftCounter <= shiftCounter + 1;
-
-					if (shiftCounter == 4'd3)
-						state <= HOLD;
-				end
-
-				// Don't know if this state is still needed. Should just go back to idle anyway.
-				HOLD: begin
-					if (tValidIn == 1'b0)
-						state <= IDLE;
-				end
-			endcase
-		end
-	end
-
-	// Counter used to create SCLK
-	always@(posedge sysRstIn or posedge sysClkIn)
-	begin
-		if (sysRstIn) begin
-			counter <= 0;
-		end else begin
-			if (state == SEND && ~(counter == COUNTER_MID && shiftCounter == 8)) begin
-				if (counter == COUNTER_MAX)
-					counter <= 0;
-				else
-					counter <= counter + 1;
-			end else begin
-				counter <= 0;
-			end
-		end
-	end
-
-	// Outputs
-	assign SCLK = (counter < SCLK_DUTY_CYCLE) | SS;
-	assign MOSI = mosiTemp | SS | (state == HOLD_SS ? 1'b1 : 1'b0);
-	assign SS   = (state != SEND && state != HOLD_SS) ? 1'b1 : 1'b0;
-	assign tReadyOut = (state == IDLE && tValidIn == 1'b0) ? 1'b1 : 1'b0;
+        SEND: begin
+          if (counter == COUNTER_MID)
+          begin
+            mosiR <= shiftData[7];
+            shiftData <= {shiftData[6:0], 1'b0};
+            if (shiftCounter == 8)
+            begin
+              shiftCounter <= 0;
+              spiState     <= IDLE;
+            end else
+            begin
+              shiftCounter <= shiftCounter + 1;
+            end
+          end
+        end
+      endcase
+    end
+  end
 
 endmodule
